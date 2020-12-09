c94f0ef3f7ff45c92ae1c5013649137e2a8a957e
paupalou <paupalou@gmail.com>
2020-12-09 09:00:42 +0100

    Append reducers and sagas to root

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
+
 import counterReducer from './modules/counter/reducer';
+import productReducer from './modules/products/reducer';
+import cartReducer from './modules/cart/reducer';
 
 export default combineReducers({
-    counter: counterReducer
+  counter: counterReducer,
+  products: productReducer,
+  cart: cartReducer
 });
--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -1,14 +1,19 @@
 import { all, spawn, call } from 'redux-saga/effects';
+
 import { watchIncrementCounterSaga } from './modules/counter/saga';
+import { watchRequestProductListSaga } from './modules/products/saga';
+import { watchRequestAddToCartSaga } from './modules/cart/saga';
 
 export default function* allSagas() {
   const sagas = [
-      watchIncrementCounterSaga,
+    watchIncrementCounterSaga,
+    watchRequestProductListSaga,
+    watchRequestAddToCartSaga
   ];
 
   yield all(
-    sagas.map(saga =>
-      spawn(function*() {
+    sagas.map((saga) =>
+      spawn(function* () {
         while (true) {
           try {
             yield call(saga);
